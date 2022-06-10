6f2a5043a8fdca10101d8a77f5023dd5ffac44e6
Radek Musil <77067400+this-radlas@users.noreply.github.com>
2022-06-10 22:10:02 +0200

    categoryDetail

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -13,7 +13,7 @@ const Categories = () => {
     <div className="shadow-lg rounded-lg p-0 p-8 mb-8 bg-white">
       <h4 className="text-2xl font-semibold mb-3">Categories</h4>
       {categories.map((category) => (
-        <Link key={category.slug} href={`/${category.slug}`}>
+        <Link key={category.slug} href={`/category/${category.slug}`}>
           {category.name}
         </Link>
       ))}
--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
           </div>
           <div className="hidden md:float-left md:contents">
             {categories.map((category) => (
-              <Link key={category.slug} href={`/${category.slug}`}>
+              <Link key={category.slug} href={`/category/${category.slug}`}>
                 <span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">{category.name}</span>
               </Link>
             ))}
